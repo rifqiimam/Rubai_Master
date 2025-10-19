e2902e984118ec12e61311067cbc650616a7c1b9
RIFQI IMAM RAMADHAN <imamramadhan.rifqi@gmail.com>
2025-10-19 19:16:32 +0700

    first commit

---
+++ src/main/kotlin/com/rubai/rubai_master/RubaiMasterApplication.kt
@@ -0,0 +1,11 @@
+package com.rubai.rubai_master
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RubaiMasterApplication
+
+fun main(args: Array<String>) {
+	runApplication<RubaiMasterApplication>(*args)
+}
---
+++ src/test/kotlin/com/rubai/rubai_master/RubaiMasterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rubai.rubai_master
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RubaiMasterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
